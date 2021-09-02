5075f1ac92a2e4f4d91909b5015b17aa1cbeb9dc
Payal246 <payal@hastishah.com>
2021-09-02 15:39:49 +0530

    comment added

--- delete.php
+++ delete.php
@@ -2,6 +2,6 @@
 include "connection.php";
 $rid=$_POST['rid'];
 $sql=$link->prepare("delete from register where Reg_id='$rid' LIMIT 1");
-$data=$sql->execute();
+$data=$sql->execute(); // this command executes query
 
 ?>
